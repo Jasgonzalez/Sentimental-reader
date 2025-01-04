71646a2a103c1b95d0a9dd9c53e3e86e85320c13
Jason Gonzalez <jegonzalez1111@gmail.com>
2025-01-03 19:25:51 -0500

    found bug

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> 052f9df3aacaf461db78eb10fa9b12afe3b24b74
