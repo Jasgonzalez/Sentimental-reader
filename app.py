d485889adfb16653ce00b70bdf42fd2051af7a6b
Jason Gonzalez <jegonzalez1111@gmail.com>
2025-01-04 02:14:27 -0500

    flask practice

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template('home.html')
+
+
+@app.route("/analyze", methods=['POST'])
+def analyze():
+    user_input = float(request.form['user_input'])
+
+    if user_input > 0:
+        return "True"
+    
+    else:
+        return "false"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
